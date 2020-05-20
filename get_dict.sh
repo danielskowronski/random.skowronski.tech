d54ec58213a3a8020112e8c11b1eed0decb38c18
Daniel Skowroński <daniel@dsinf.net>
2020-05-20 15:36:19 +0200

    fix get_dict.sh

--- get_dict.sh
+++ get_dict.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+if [[ -f slowa.txt ]]; then
+	exit 0
+fi
+
 wget http://www.math.uni.wroc.pl/~hebisch/prog/pol.lst -O slowa-iso.txt
 iconv -f ISO-8859-2 slowa-iso.txt -t UTF-8 -o slowa.txt
 rm slowa-iso.txt

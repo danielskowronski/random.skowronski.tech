c64e6873e1f1b3712f1c391c680786770307f3af
Daniel Skowroński <daniel@dsinf.net>
2021-03-17 23:48:18 +0100

    rearrange page, add numbers generator, make strings longer and generate more results in one go
    * rearrange page
    * offline jQuery (added to get_dict.sh)
    * add random numbers 0-1k and 0-1M
    * generate more words
    * more words in passphrase
    * replace static 32 chars long with sections for 16, 32, 64, 128 and 256 chars long ones

--- get_dict.sh
+++ get_dict.sh
@@ -6,3 +6,5 @@ fi
 wget http://www.math.uni.wroc.pl/~hebisch/prog/pol.lst -O slowa-iso.txt
 iconv -f ISO-8859-2 slowa-iso.txt -t UTF-8 -o slowa.txt
 rm slowa-iso.txt
+
+wget https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.5.0.min.js

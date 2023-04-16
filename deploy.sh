17e31886f8d80dc28182b79c4fdc0e3d194a8af5
Daniel Skowroński <daniel@skowron.ski>
2023-04-16 08:16:43 +0200

    add cache purge to deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
 aws s3 sync --delete ./app/ s3://random.skowronski.tech
+
+curl -X POST "https://api.cloudflare.com/client/v4/zones/6443f45f13776c3183e79b68443b4201/purge_cache" \
+     -H "Authorization: Bearer ${CLOUDFLARE_API_TOKEN}" \
+     -H "Content-Type: application/json" \
+     --data '{"purge_everything":true}'
